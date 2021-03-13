950b38870cbe7c6dcf8fae1d8b354fc002c70750
Ilya Kryzhevich <kruzhevich.ilya@yandex.ru>
2021-03-14 00:29:07 +0300

    feat: complete task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,56 @@
+function checker(array) {
+  if (!array || array.join('').length === 0) {
+    return false;
+  } else {
+    return true;
+  }
+}
+
+
 
 exports.min = function min (array) {
-  return 0;
+  checker(array)  
+  if (checker(array)) {
+    let min = array[0];
+  
+    array.forEach((item) => {
+      if (item < min) {
+        min = item;
+      }
+    })
+    return min
+  } else {
+    return 0;
+  }
 }
 
 exports.max = function max (array) {
+  checker(array)  
+  if (checker(array)) {
+  let max = array[0];
+  
+  array.forEach((item) => {
+    if (item > max) {
+      max = item;
+    }
+  })
+  return max
+} else {
   return 0;
+  }
 }
 
+
 exports.avg = function avg (array) {
+  checker(array)  
+  if (checker(array)) {
+  let avg = 0;
+  array.forEach((item) => {
+    avg += item
+  })
+  return avg / array.length
+} else {
   return 0;
+  }
 }
+
